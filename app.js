a2b7a2b47be7df2677bff76f13d1da8ce127251e
Diana <diana.wanjiru@student.moringaschool.com>
2022-09-01 10:35:42 +0300

    add js file

---
+++ app.js
@@ -0,0 +1,35 @@
+//selectors
+const todoInput = document.querySelector(".todo-input")
+const todoButton = document.querySelector(".todo-button")
+const todoList = document.querySelector(".todo-list")
+//eventlisteners
+todoButton.addEventListener("click", addToDo); 
+
+//functions
+function addToDo(event){
+    //prevent form from submitting
+    event.preventDefault();
+    //ToDo Div
+    const todoDiv = document.createElement("div")
+    todoDiv.classList.add("todo");
+    //Todo Li
+    const newTodo = document.createElement("li");
+    newTodo.innerText="hey";
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo);
+    //CHECK BUTTON
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton );
+
+    //TRASH BUTTON
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+
+    //append the todoDiv to the ul
+    todoList.appendChild(todoDiv); 
+
+}
