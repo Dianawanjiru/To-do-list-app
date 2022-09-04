5cb50afea6d803c0054efe148bdd745cc45d6418
Diana <diana.wanjiru@student.moringaschool.com>
2022-09-04 18:07:04 +0300

    modifying the js

--- app.js
+++ app.js
@@ -89,6 +89,15 @@ function deleteCheck(e){
     }
 
     function saveLocalTodos(todo) {
-        
+        //check if things already exist here
+        let todos;
+        if(localStorage.getItem("todos")===null){
+            todos=[];
+        } else {
+            todos=JSON.parse(localStorage.getItem("todos"))
+        }
+        todos.push(todo);
+        localStorage.setItem("todos", JSON.stringify("todos"));
+
 
     }
